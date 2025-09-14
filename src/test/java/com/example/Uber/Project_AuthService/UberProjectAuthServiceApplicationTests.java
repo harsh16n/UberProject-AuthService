939f060f1128ec6b3b7504bae8040ebc3183e277
harsh tyagi <harshtyagi047@gmail.com>
2025-09-14 16:30:28 +0530

    first commit

---
+++ src/test/java/com/example/Uber/Project_AuthService/UberProjectAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber.Project_AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
